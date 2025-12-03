8946aeaf4821a57b54f994a473b8c84078048be4
Felipe Alferez <alferezfelipe@gmail.com>
2025-12-03 17:28:42 -0500

    feat(books-transport): add transport layer and simple method to get all books

---
+++ internal/transport/book_transport.go
@@ -0,0 +1,30 @@
+package transport
+
+import (
+	"bare-crud/internal/service"
+	"encoding/json"
+	"net/http"
+)
+
+type BookHandler struct {
+	service *service.Service
+}
+
+func New(s *service.Service) *BookHandler {
+	return &BookHandler{
+		service: s,
+	}
+}
+
+func (h *BookHandler) HandleBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		books, err := h.service.GetAll()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type: application/json")
+		json.NewEncoder(w).Encode(books)
+	}
+}
